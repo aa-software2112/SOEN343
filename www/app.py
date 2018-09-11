81b0159275d8e525f2b4e3780303ad87866b9342
Samuel Gosselin <samuelgosselin1@hotmail.com>
2018-09-10 22:48:03 -0400

    example obj from backend to frontend

--- www/app.py
+++ www/app.py
@@ -7,5 +7,12 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', title='SOEN343')
 
+@app.route('/example')
+def showObject():
+    # creating a dictionnary object on the fly
+    dictionary = {"Title":"TIME", "Publisher":"Time (May 13 2008)", "Language":"English", "ISBN-10":1603200185}
+    # rendering the template and setting the objToDisplay in the exampleBackendToFrontend.html to the dictionary
+    return render_template('exampleBackendToFrontend.html', objToDisplay=dictionary)
+
 if __name__ == "__main__":
     app.run()
