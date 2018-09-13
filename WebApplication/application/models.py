f73c35747c869a151bbbd5c6cd15e04a5458988b
Alexander Chung <achung95@hotmail.com>
2018-09-13 09:50:52 -0400

    #2  Created a model file
    1.Added library set ups in __init__ file
    2.Created a models file with a

---
+++ WebApplication/application/models.py
@@ -0,0 +1,10 @@
+from application import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fname = db.Column(db.String(64), index=True, unique=True)
+
+
+    def __repr__(self):
+        return '<User: {}>'.format(self.fname)
