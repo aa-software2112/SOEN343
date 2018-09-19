4e8ca6e3f3987182ed1c8d2089320f36d1770771
aa-software2112 <a_dreoli@encs.concordia.ca>
2018-09-18 20:17:14 -0400

    test

--- WebApplication/application/routes.py
+++ WebApplication/application/routes.py
@@ -4,9 +4,9 @@ import random
 
 # All routes from route folder are imported here...
 from application.routesToPages import routeToDBDeleteRecord
-from application.routesToPages import routeToIndex
+from application.routesToPages import routeToDBInsertRecord
 from application.routesToPages import routeToDBQuerryRecord
+from application.routesToPages import routeToExample
+from application.routesToPages import routeToIndex
 from application.routesToPages import routeToLogin
 from application.routesToPages import routeToSetCookies
-from application.routesToPages import routeToExample
-from application.routesToPages import routeToDBInsertRecord
