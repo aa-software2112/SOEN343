b2b35dcbf6731b69c1093f3b8bd652bbb97c5a55
aa-software2112 <a_dreoli@encs.concordia.ca>
2018-09-20 10:52:56 -0400

    Removed filesystems to detect uppercase

--- WebApplication/application/Controllers/UserController.py
+++
@@ -1,9 +0,0 @@
-from application.Controllers.Controller import Controller
-
-class UserController(Controller):
-	
-	def __init__(self, database):
-		Controller.__init__(self, database)
-
-	def exampleUserControllerFunction(self):
-		print("User Controller")
--- WebApplication/application/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-import random
-
-# All routes from route folder are imported here...
-from application.RoutesToPages import routeToExample
-from application.RoutesToPages import routeToIndex
-from application.RoutesToPages import routeToLogin
-from application.RoutesToPages import routeToSetCookies
