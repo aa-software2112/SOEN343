4936d20db5d86ec52ce08dbe271871cb544e2cd0
SajeelS <sajeel155@yahoo.com>
2018-09-23 14:08:06 -0400

    #33 Added a route to process signup form

--- WebApplication/application/RoutesToPages/routeToClientSignup.py
+++ WebApplication/application/RoutesToPages/routeToClientSignup.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from flask import request
 from application import app
 from application import userController, adminController
 from application import databaseObject as db
@@ -6,4 +7,12 @@ import random
 
 @app.route('/clientSignup')
 def clientSignup():
+	return render_template('clientSignup.html')
+
+@app.route('/signupForm', methods=['POST'])
+def processSignup():
+	print(request.method)
+	for k, v in request.form.items():
+		print(k)
+		print(v)
 	return render_template('clientSignup.html')
