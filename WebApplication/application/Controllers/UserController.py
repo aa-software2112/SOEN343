3daf7533a408b65f3f613fe736b87565cb209b77
SAA9 <s.ali3@protonmail.com>
2018-09-24 17:14:05 -0400

    First draft for issues 47-50
    This commit includes first draft of function:
    in UserController:
    - getClientByUsername,
    - getClientByEmail,
    - getClientByPassword,
    - createClient
    
    in AdminController
    - createAdmin

--- WebApplication/application/Controllers/UserController.py
+++ WebApplication/application/Controllers/UserController.py
@@ -7,3 +7,60 @@ class UserController(Controller):
 
 	def exampleUserControllerFunction(self):
 		print("User Controller")
+
+	#function takes self and a string "username" to get the user from the client table.
+	#returns none with message or displays client information and returns client row.
+	def getClientByUsername(self, username):
+		#to delete: sqlGetClientByUsername = ''' Select * From client Where username = ? , ('''username''',)'''
+		getClientCursor = self.db.executeQuery("Select * From client Where username = ?", (username,))
+		foundClient = getClientCursor.fetchone()
+
+		if foundClient == None:
+			print("There are no client with given Username")
+		else:
+			print(foundClient)
+		
+		return foundClient
+
+	#function takes self and a string "email" to get the user from the client table.
+	#returns none with message or displays client information and returns client row.
+	def getClientByEmail(self, email):
+		getClientCursor = self.db.executeQuery("Select * From client Where username = ?", (email,))
+		foundClient = getClientCursor.fetchone()
+		
+		if foundClient == None:
+			print("There are no client with given Username")
+		else:
+			print(foundClient)
+		
+		return foundClient
+
+	#function takes self and a string "email" to get the user from the client table.
+	#returns none with message or displays client information and returns client row.
+	def getClientByPassword(self, username, password):
+		getClientCursor = self.db.executeQuery("Select * From client Where username = ? AND password = ?", (username, password))
+		foundClient = getClientCursor.fetchone()
+		
+		if foundClient == None:
+			print("There are no client with given Username")
+		else:
+			print(foundClient)
+		
+		return foundClient
+
+	#function takes self and a values to create
+	#creates a new client into the client table
+	def createClient(self,firstName,lastName,physicalAddress,email,phoneNumber,username,password,isAdmin,isLogged,lastLogged):
+		getClientByUsername = self.getClientByUsername(username)
+		getClientByEmail = self.getClientByEmail(username)
+		
+		if getClientByUsername == None & getClientByEmail == None:
+			sqlInsertClient = ''' INSERT INTO client(firstName,lastName,physicalAddress,email,phoneNumber,username,password,isAdmin,isLogged,lastLogged)
+					VALUES(?,?,?,?,?,?,?,?,?,?) '''
+		
+			client = (firstName,lastName,physicalAddress,email,phoneNumber,username,password,isAdmin,isLogged,lastLogged)
+			self.db.executeQuery(sqlInsertClient, client)
+		else:
+			getClientByUsername
+
+
