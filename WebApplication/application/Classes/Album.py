28715f4a2c6f647bd02ee6d83610e4d94885c043
Samuel Gosselin <samuelgosselin1@hotmail.com>
2018-10-05 17:03:43 -0400

    Added Album, Magazne and Movie classes

---
+++ WebApplication/application/Classes/Album.py
@@ -0,0 +1,8 @@
+class Album():
+    def __init__(self,arguments):
+        self.type = arguments[0]
+        self.title = arguments[1]
+        self.artist = arguments[2]
+        self.label = arguments[3]
+        self.releaseDate = arguments[4]
+        self.ASIN = arguments[5]
---
+++ WebApplication/application/Classes/Movie.py
@@ -0,0 +1,11 @@
+class Movie():
+    def __init__(self, arguments):
+        self.title = arguments[0]
+        self.director = arguments[1]
+        self.producers = arguments[2]
+        self.actors = arguments[3]
+        self.language = arguments[4]
+        self.subtitles =arguments[5]
+        self.dubbed = arguments[6]
+        self.releaseDate = arguments[7]
+        self.runtime = arguments[8]
