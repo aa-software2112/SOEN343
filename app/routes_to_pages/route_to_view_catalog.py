553662d169a5c14a5007428c5cffca587de6790a
AleexCh <achung95@hotmail.com>
2018-11-05 19:52:23 -0500

    #189 Added privilege
    Small addition to handle if user is admin or client to call the correct controller.

--- app/routes_to_pages/route_to_view_catalog.py
+++ app/routes_to_pages/route_to_view_catalog.py
@@ -1,13 +1,15 @@
 from flask import render_template, g, session, redirect, request, flash
 from app import app
 from app.common_definitions.helper_functions import login_required
-from app import clientController
+from app import clientController, adminController
 
 @app.route('/viewCatalog', methods=['GET', 'POST'])
 @login_required
 def viewCatalog():
-
-    dict_of_catalogs = clientController.view_inventory()
+    if g.user["_is_admin"] == 1:
+        dict_of_catalogs = clientController.view_inventory()
+    else:
+        dict_of_catalogs = adminController.view_inventory()
     # comment this out when fully implemented
     # for catalog_name in dict_of_catalogs.keys():
     #   print("*****\nDictionary: {}\n*****".format(catalog_name))
