2683ce375c691637abb8afeea4d112c36bb89113
Jason Chung <jasonckc07@gmail.com>
2018-11-06 15:10:51 -0500

    Added Book and Movie search box and filters

--- app/routes_to_pages/route_to_view_catalog.py
+++ app/routes_to_pages/route_to_view_catalog.py
@@ -1,7 +1,8 @@
 from flask import render_template, g, session, redirect, request, flash
 from app import app
 from app.common_definitions.helper_functions import login_required
-from app import clientController, adminController
+from app import clientController, adminController, catalog_controller
+from app.controllers.catalog_controller import CatalogController
 
 @app.route('/viewCatalog', methods=['GET', 'POST'])
 @login_required
@@ -16,12 +17,25 @@ def viewCatalog():
     #  dict_of_objects = dict_of_catalogs[catalog_name]
     # for object_id, media_object in dict_of_objects.items():
     #    print ("ID {} OBJ {}".format(object_id, media_object))
+    
+    # Load the catalog filters when the viewCatalog page loads
+    book_filters = catalog_controller.get_filters(CatalogController.BOOK_TYPE)
+    movie_filters = catalog_controller.get_filters(CatalogController.MOVIE_TYPE)
+    magazine_filters = catalog_controller.get_filters(CatalogController.MAGAZINE_TYPE)
+    album_filters = catalog_controller.get_filters(CatalogController.ALBUM_TYPE)
 
-    return render_template('view_catalog.html', dict_of_catalogs=dict_of_catalogs)
+    return render_template('view_catalog.html', 
+        dict_of_catalogs=dict_of_catalogs, 
+        book_filters = book_filters,
+        movie_filters = movie_filters,
+        magazine_filters = magazine_filters,
+        album_filters = album_filters
+    )
 
-@app.route('/viewCatalog/getSearchFilters', methods=['GET', 'POST'])
-def getCatalogType():
-    type = int(request.form["catalog_type"])
-    print("CatalogType", type)
-    return redirect('/viewCatalog')
-
+# @app.route('/viewCatalog/getSearchFilters', methods=['GET', 'POST'])
+# def getCatalogType():
+#     type = int(request.form["catalog_type"])
+#     print("CatalogType", type)
+#     test = "Testing"
+#     render_template('includes/book_search_box.html', test=test)
+#     return redirect('viewCatalog')
