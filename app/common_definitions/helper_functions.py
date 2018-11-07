92e764ed5019008af00020571a73b4a027f38c1d
Bilal101 <35821680+Bilal101@users.noreply.github.com>
2018-11-07 17:45:24 -0500

    Changed the filter function to folllow the design .
    Changed the filter function which was taking two dic as parameter now catalog record work as list , also added comments for the functions

--- app/common_definitions/helper_functions.py
+++ app/common_definitions/helper_functions.py
@@ -7,8 +7,6 @@ import time
 import datetime
 
 
-
-
 def convert_epoch_to_datetime(epoch_time):
     """Returns time in 'month/day/year hour:minute:second' format given the epoch time"""
     return time.strftime("%m/%d/%Y %H:%M:%S %Z", time.localtime(epoch_time))
@@ -63,50 +61,47 @@ def admin_required(f):
     return admin_check
 
 
-
 def filter(criteria, catalog_records):
     lst = []
-    for k, v in catalog_records.items():
+    #takes two parameter as input criteria as dictionary and catalog_records(example book )as list and match the criteria value in
+    #catalog record to the search value if match is found add it inside the empty list and return it .
+    for v in catalog_records:
         for a, b in criteria.items():
-            if v.catalog_records.__dic__[a] == b:
+            if v.__dict__[a] == b:
                 print("Match found! entering into the list")
                 lst.append(v)
-
 
                 break
 
     return lst
 
 
-
-
-  
-#Sorts a list of dictionnary (i.e. last_searched_list) based on criteria provided (i.e sort_key_values)
-#sort_key_values = holds criteria dictionnary of ascending/descenting, Attribute. Ex: {"ascending": "Title"}
-#last_searched_list = holds list of objects
+# Sorts a list of dictionnary (i.e. last_searched_list) based on criteria provided (i.e sort_key_values)
+# sort_key_values = holds criteria dictionnary of ascending/descenting, Attribute. Ex: {"ascending": "Title"}
+# last_searched_list = holds list of objects
 def sort_records(sort_key_values, last_searched_list):
+    # gets the order to which it will be sorted (ascending or descending) from the sort_key_values criteria
+    sort_order = list(sort_key_values.keys())[0]
 
-    #gets the order to which it will be sorted (ascending or descending) from the sort_key_values criteria
-    sort_order =  list(sort_key_values.keys())[0]
-
-    #Gives the attributes to sort the book objects to. Taken from the sort_key_values criteria
+    # Gives the attributes to sort the book objects to. Taken from the sort_key_values criteria
     sort_attribute = list(sort_key_values.values())[0]
 
-    #Holds boolean which will be used to sort inorder or in reverse order   
+    # Holds boolean which will be used to sort inorder or in reverse order
     if sort_order is "ascending":
         reverse_order = False
     elif sort_order is "descending":
         reverse_order = True
-    else: 
+    else:
         print("criteria value isn't properly defined in terms of ascending or descending")
         empty_list = []
         return empty_list
 
-    #Sorts the catalogs items by its value in reverse order or in order.
-    #Note: since python3 doesn't allow unpacking: https://www.python.org/dev/peps/pep-3113/
-    #An example: https://stackoverflow.com/questions/72899/how-do-i-sort-a-list-of-dictionaries-by-a-value-of-the-dictionary
-    last_searched_list_sorted = sorted(last_searched_list, key=lambda record: record.__dict__[sort_attribute], reverse=reverse_order)
-    
+    # Sorts the catalogs items by its value in reverse order or in order.
+    # Note: since python3 doesn't allow unpacking: https://www.python.org/dev/peps/pep-3113/
+    # An example: https://stackoverflow.com/questions/72899/how-do-i-sort-a-list-of-dictionaries-by-a-value-of-the-dictionary
+    last_searched_list_sorted = sorted(last_searched_list, key=lambda record: record.__dict__[sort_attribute],
+                                       reverse=reverse_order)
+
     return last_searched_list_sorted
 
 
@@ -125,4 +120,3 @@ def search_catalog(catalog, search_string):
             lst.append(v)
 
     return lst
-
