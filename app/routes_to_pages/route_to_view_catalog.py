261b744d12a24558ff7bd072e3fc1273c5cbe5ec
Jason Chung <jasonckc07@gmail.com>
2018-11-07 18:15:03 -0500

    Fixed variable name and change records dict to list

--- app/routes_to_pages/route_to_view_catalog.py
+++ app/routes_to_pages/route_to_view_catalog.py
@@ -4,13 +4,16 @@ from app.common_definitions.helper_functions import login_required
 from app import clientController, adminController, catalog_controller
 from app.controllers.catalog_controller import CatalogController
 
+# Needs to be redone
 @app.route('/viewCatalog', methods=['GET', 'POST'])
 @login_required
 def viewCatalog():
-    if g.user["_is_admin"] == 1:
-        dict_of_catalogs = adminController.view_inventory()
-    else:
-        dict_of_catalogs = clientController.view_inventory()
+    # if g.user["_is_admin"] == 1:
+    #     dict_of_catalogs = adminController.view_inventory()
+    # else:
+    #     dict_of_catalogs = clientController.view_inventory()
+
+
     # comment this out when fully implemented
     # for catalog_name in dict_of_catalogs.keys():
     #   print("*****\nDictionary: {}\n*****".format(catalog_name))
@@ -18,19 +21,8 @@ def viewCatalog():
     # for object_id, media_object in dict_of_objects.items():
     #    print ("ID {} OBJ {}".format(object_id, media_object))
     
-    # Load the catalog filters when the viewCatalog page loads
-    book_filters = catalog_controller.get_filters(CatalogController.BOOK_TYPE)
-    movie_filters = catalog_controller.get_filters(CatalogController.MOVIE_TYPE)
-    magazine_filters = catalog_controller.get_filters(CatalogController.MAGAZINE_TYPE)
-    album_filters = catalog_controller.get_filters(CatalogController.ALBUM_TYPE)
 
-    return render_template('view_catalog.html', 
-        # dict_of_catalogs=dict_of_catalogs, 
-        book_filters = book_filters,
-        movie_filters = movie_filters,
-        magazine_filters = magazine_filters,
-        album_filters = album_filters
-    )
+    return render_template('view_catalog.html')
 
 # Separate the singular view catalog page into its respective page
 @app.route('/viewCatalog/viewCatalogTab', methods=['GET', 'POST'])
@@ -39,22 +31,22 @@ def viewCatalogTab():
     if (type == 1):
         book_filters = catalog_controller.get_filters(CatalogController.BOOK_TYPE)
         all_records = catalog_controller.get_records_by_catalog(CatalogController.BOOK_TYPE)
-        return render_template('view_books.html', books = all_records, book_filters = book_filters)
+        return render_template('view_books.html', records = list(all_records.values()), book_filters = book_filters)
 
     elif (type == 2):
         movie_filters = catalog_controller.get_filters(CatalogController.MOVIE_TYPE)
         all_records = catalog_controller.get_records_by_catalog(CatalogController.MOVIE_TYPE)
-        return render_template('view_movies.html', movies = all_records, movie_filters = movie_filters)
+        return render_template('view_movies.html', records = list(all_records.values()), movie_filters = movie_filters)
 
     elif (type == 3):
         magazine_filters = catalog_controller.get_filters(CatalogController.MAGAZINE_TYPE)
         all_records = catalog_controller.get_records_by_catalog(CatalogController.MAGAZINE_TYPE)
-        return render_template('view_magazines.html', magazines = all_records, magazine_filters = magazine_filters)
+        return render_template('view_magazines.html', records = list(all_records.values()), magazine_filters = magazine_filters)
 
     elif (type == 4):
         album_filters = catalog_controller.get_filters(CatalogController.ALBUM_TYPE)
         all_records = catalog_controller.get_records_by_catalog(CatalogController.ALBUM_TYPE)
-        return render_template('view_albums.html', albums = all_records, album_filters = album_filters)
+        return render_template('view_albums.html', records = list(all_records.values()), album_filters = album_filters)
 
 # To-do
 @app.route('/viewCatalog/search', methods=['GET', 'POST'])
