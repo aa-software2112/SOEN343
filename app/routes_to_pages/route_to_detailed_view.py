95128ab36a770dec501a9d5e7f01f8ace95763d1
SajeelS <sajeel155@yahoo.com>
2018-11-07 15:50:46 -0500

    Added handler for detailed view and next button

---
+++ app/routes_to_pages/route_to_detailed_view.py
@@ -0,0 +1,29 @@
+from flask import render_template, g, session, redirect, request, flash
+from app import app
+from app.common_definitions.helper_functions import login_required
+from app import clientController, adminController
+
+@app.route('/detailedView', methods=['GET', 'POST'])
+@login_required
+def detailedView():
+    id = request.form["id"]
+    catalog_type = request.form["type"]
+    if g.user["_is_admin"] == 1:
+        catalog_entry = adminController.get_catalog_entry_by_id(catalog_type, int(id))
+    else:
+        catalog_entry = clientController.view_inventory()
+
+    #uncomment  when template for detailed view is defined
+    #return render_template('detailed_view.html', catalog_entry=catalog_entry)
+
+@app.route('/nextDetailedView', methods=['GET', 'POST'])
+@login_required
+def nextDetailedView():
+    id = request.form["id"]
+    if g.user["_is_admin"] == 1:
+        next_catalog_entry = adminController.get_next_item(int(id))
+    else:
+        next_catalog_entry = clientController.get_next_item(int(id))
+
+    #uncomment  when template for detailed view is defined
+    #return render_template('detailed_view.html', next_catalog_entry=next_catalog_entry)
