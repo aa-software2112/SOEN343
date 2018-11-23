6630e808434e87c20e7c6fca7ca21a3807590beb
Samuel Gosselin <samuelgosselin1@hotmail.com>
2018-11-22 19:49:15 -0500

    front end done

--- app/routes_to_pages/route_to_loans.py
+++ app/routes_to_pages/route_to_loans.py
@@ -5,19 +5,17 @@ from app import client_controller, catalog_controller, admin_controller
 from app.classes.loan import Loan
 
 
-
 @app.route('/myLoans')
 @login_required
 def my_loans():
 	"""Function that get the user's loans and display them on a page"""
 
-
-	#user_loans = client_controller.get_loaned_items(g.user["_id"])
+	# user_loans = client_controller.get_loaned_items(g.user["_id"])
 	loan1 = Loan(client_controller.get_client_by_username('antman')[0],
 				 catalog_controller.get_catalog_entry_by_id("1", 1))
 	loan2 = Loan(admin_controller.get_admin_by_username('catwoman')[0],
 				 catalog_controller.get_catalog_entry_by_id("2", 1))
-	user_loans = [loan1,loan2]
+	user_loans = [loan1, loan2]
 
 	if len(user_loans) == 0:
 		err = "There are no items to return"
@@ -29,38 +27,55 @@ def my_loans():
 
 @app.route('/returnLoans', methods=['POST'])
 def return_loans():
-
 	print(request.form)
-	#returns the id(selected) as a list
+	# returns the id(selected) as a list
 	return_items_id = request.form.getlist('id')
 	print(return_items_id)
 	client_controller.return_loaned_items(return_items_id, g.user["_id"])
 	err = None
-	if len(return_items_id) == 0 :
+	if len(return_items_id) == 0:
 		err = "Please, select items to return"
 	return render_template('return_loans.html', error=err)
+
 
 @app.route('/transactionHistory')
 @login_required
 @admin_required
 def display_loans():
-	# loan used for testing the feature, while waiting for get all loans as list
-	loan1 = Loan(client_controller.get_client_by_username('antman')[0],catalog_controller.get_catalog_entry_by_id("1",1))
-	loan2 =Loan(admin_controller.get_admin_by_username('catwoman')[0],catalog_controller.get_catalog_entry_by_id("2",1))
+	# testing variables
 
+	# loan used for testing the feature, while waiting for get all loans as list
+	# loan1 = Loan(client_controller.get_client_by_username('antman')[0],
+	# 			 catalog_controller.get_catalog_entry_by_id("1", 1))
+	# loan2 = Loan(admin_controller.get_admin_by_username('catwoman')[0],
+	# 			 catalog_controller.get_catalog_entry_by_id("2", 1))
+
+	# By following View_Transaction_History_Communication_Diagram
+	list = admin_controller.view_transaction_history()
+
+	error = None
+	if len(list) == 0:
+		error = "No transactions"
 
 	return render_template('transaction_history.html', epoch_converter=convert_epoch_to_datetime,
-						   all_loans=[loan1, loan2])
+						   all_loans=list, err=error)
+
 
 @app.route('/searchTransaction', methods=['POST'])
 @login_required
 @admin_required
 def search_transaction():
-	search_option = request.form['search-option']
-	print(search_option)
+	key = request.form['search-option']
 	value = request.form['search-keyword']
-	print(value)
-	loan = Loan(admin_controller.get_admin_by_username('catwoman')[0],catalog_controller.get_catalog_entry_by_id("2",1))
-	# get the proper loan list from the form
-	return render_template('transaction_history.html' , all_loans= [], search_loan_list = [loan], err=None,
-						   epoch_converter=convert_epoch_to_datetime)
+
+	# dictionary from the Search_Transaction_History_Communication_Diagram
+	search_Transaction_KeyValue = {key: value}
+
+	list = admin_controller.search_TransactionBy(search_Transaction_KeyValue)
+
+	error = None
+	if len(list) == 0:
+		error = "No transactions"
+
+	return render_template('transaction_history.html', all_loans=[], search_loan_list=list,
+						   epoch_converter=convert_epoch_to_datetime, err=error)
